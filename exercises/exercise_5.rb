7bf83e0f4d22c2ae39c49129abea1df9e3ac4020
Julie Fitzpatrick <juliefitzpatrick@Julies-MacBook-Air.local>
2022-01-05 19:45:57 -0330

    this commit adds the code for exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+sum_revenue = Store.sum(:annual_revenue)
+puts sum_revenue
+
+average_revenue = Store.average(:annual_revenue)
+puts average_revenue
+
+@stores_million = Store.where(['annual_revenue > ?', 1000000]).count
+puts @stores_million
